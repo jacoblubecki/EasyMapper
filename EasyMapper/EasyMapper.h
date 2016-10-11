59421b87fcd489f092601b88ae8ed632793c5b85
Jacob Lubecki <jacoblubecki@gmail.com>
2016-10-11 18:49:24 -0500

    Initial Commit

---
+++ EasyMapper/EasyMapper.h
@@ -0,0 +1,19 @@
+//
+//  EasyMapper.h
+//  EasyMapper
+//
+//  Created by Jacob Lubecki on 10/11/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasyMapper.
+FOUNDATION_EXPORT double EasyMapperVersionNumber;
+
+//! Project version string for EasyMapper.
+FOUNDATION_EXPORT const unsigned char EasyMapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyMapper/PublicHeader.h>
+
+
